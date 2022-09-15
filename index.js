5ac84532e1313448ed755148ca41f3e09738f429
DKFAEL <rafaelnunesemp@hotmailcom>
2022-09-15 19:33:13 -0300

    MY JAVA

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Tipos primitivos
+ *Variaveis no JavaScript
+ * Operadores matematicos do Javascript
+ */
+
+//serve para escrever mensagem no console
+
+//String  - textos
+//number -- números
+
+console.log("HELLO GIRL")
+console.log(5)
+console.log(43.76)
+console.log(true)
+console.log(false)
+
+
+// Variáveis
+let mensagem = 'Soulcode' // declaração da variavél
+
+console.log(mensagem)
+
+mensagem = 'Soucode BC 25 JFS'
+
+console.log(mensagem)
+
+
+
+console.log(mensagem)
