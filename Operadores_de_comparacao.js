ad165ec28e991632d2a5ea73fad7e2c47003821c
DKFAEL <rafaelnunesemp@hotmail.com>
2022-09-19 18:06:33 -0300

    OPERADORES DE COMPARAÇAOE LOGICOS

---
+++ Operadores_de_comparacao.js
@@ -0,0 +1,40 @@
+/**
+ * Operadores de comparação
+ */
+
+//Verdadeiroou   (true) ou  falso (false)
+
+// igualdade
+
+const msg = 'eu sou uma mensagem'
+const msg2 = ' eu sou a segunda mensagem'
+
+console.log(msg == msg2) // (False) == 
+const num = 5
+const num2 = 5
+console.log (num == num2) // true
+
+let a = 78
+let b = '78'
+console.log(a==b) // true
+
+// idênticos
+console.log (a === b) // false
+
+// Diferença
+console.log(msg != msg2) // true !== (Diferença)
+console.log(a != b) // false
+console.log(a !==b) // true
+
+a = 56
+b = 30
+let c = 500
+let d = 500
+console.log (a > 30 )  // true      ( >  ) MAIOR QUE...
+console.log (c >= d)// true     ( >=  )  MAIOR OU IGUAL QUE...
+
+
+console.log (3 < 7.5) // true    ( <  ) MENOR QUE..
+console.log (7.5< 7.5) // false   ( <= )  MENOR OU IGUAL QUE...
+console.log (8 <= 8) // true
+
---
+++ operadores_logicos.js
@@ -0,0 +1,59 @@
+/**
+ * Operadores Logicos
+ */
+
+// Operador E  (REPRESENTADO POR ->  &&)
+
+// v -> verdadeiro
+// f - falso
+/**
+ *  V V -> VERDADEIRO
+ * F V - > FALSO       PRECISA QUE TODOS SEJAM VERDADEIROS
+ * V F -> FALSO
+ * F F -> FALSO
+ */
+
+//média precisa ser maior ou igual 7 e precisa ter, no minimo, 80%  de presença
+const media =(3.5 + 6 + 10) / 3
+const presença = 0.7
+console.log (media >= 7 && presença >= 0.8) // false 
+
+const media2 =(7. + 10 + 10) / 3
+const presença2 = 0.95
+console.log (media2 >= 7 && presença2 >= 0.8) // true
+
+//OPERADOR Ou (REPRESENTADO POR  ||)
+
+/**
+ * V V -> V
+ * F V -> V            SO PRECISA DE UMA VERDADEIRA
+ * V F -> V
+ * F F -> F
+ */
+console.log(4> 7 || 3 < 2)// false
+console.log(5 > 3.5 || 7 > 8) // true
+
+
+const estado = 'CE'
+console.log( estado == 'SP' || estado == 'MG')
+
+const idade = 15
+console.log(idade >= 16 || idade < 60) // true
+
+
+// OPERADOR NÃO REPRESENTADO PELO  -> !
+
+/**
+ * not V -> F
+ * not F ->  V            (NEGAÇÃO)
+ */
+
+ const valor = 5 < 3
+ console.log (!valor )
+ console.log (!true)
+ console.log (!false)
+
+
+const logado = false
+
+if (!logado) { 'você não pode ecessar o meu SubmitEvent, sai daqui'}
