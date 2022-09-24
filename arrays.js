bf0bcc9196a1fd305b47eb960c8a76eed8d98222
DKFAEL <rafaelnunesemp@hotmail.com>
2022-09-24 01:14:37 -0300

    ARRAYS

---
+++ arrays.js
@@ -0,0 +1,25 @@
+/**
+ * Arrays ou vetores é um tipo especial que te permite armazenar diversos valores
+ * em apenas um local
+ */
+
+// criando um arrays
+/*let arr = new Array()     Array -> []
+let arr2 = new Array(5) */
+
+/**
+ * Cada valor de um array é separado por virgula
+ * 
+ *  Dentro de um array eu posso ter vários tipos de dados
+ */
+
+/**
+ * ->Cada posição e representada por um numero
+ * -> A primeira posição de um Array é a posição/ é a 0
+ */
+let arr = [10,15, 9.8, 'Eu sou um texto dentro de uma array', 'Olá, mundo' , true, false, false]
+console.log(arr[6])
+arr [6] = 'batata'
+console.log(arr[6])
+arr [8] = 'Agora a posição 8 existe dentro de arr'
+console.log(arr)
